b062abbd8656a2c3fb498dbf0fe0eea434f8a31c
prawneel <pranil.221734@ncit.edu.np>
2024-01-11 17:47:18 +0545

    updated placeholders

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -120,7 +120,7 @@
                   <div class="avatar-img">
                     <img src="../assets/img/person/person_1.jpg" alt="">
                   </div>
-                  <span>Roger Adams</span>
+                  <span>Rushabs</span>
                 </div>
                 <span class="mai-time"></span> 4 weeks ago
               </div>
@@ -131,7 +131,7 @@
           <div class="card-blog">
             <div class="header">
               <div class="post-category">
-                <a href="#">Covid19</a>
+                <a href="#">Corona</a>
               </div>
               <a href="blog-details.html" class="post-thumb">
                 <img src="../assets/img/blog/blog_3.jpg" alt="">
